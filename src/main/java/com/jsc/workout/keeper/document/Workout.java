b9446d37ae8b22f876d6c3bbd1d2b130feb6a8dc
dbahaslouski <dbahaslouski@deem.com>
2023-02-03 01:22:29 +0300

    add `start` and `end` LocalDateTime

--- src/main/java/com/jsc/workout/keeper/document/Workout.java
+++ src/main/java/com/jsc/workout/keeper/document/Workout.java
@@ -6,18 +6,23 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-@EqualsAndHashCode(of = "workoutId")
 @Document("workouts")
+@EqualsAndHashCode(of = "workoutId")
 public class Workout {
 
     @Id
     private String workoutId;
 
     private String name;
+
+    private LocalDateTime start;
+
+    private LocalDateTime end;
 
     private List<Exercise> exercises;
 
